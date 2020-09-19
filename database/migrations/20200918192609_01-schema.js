3c85f3c255d03d89b95bb06e6878934e0edcc233
royeradames <royeraadames@gmail.com>
2020-09-18 20:51:42 -0400

    completed the tables for the database, and added a schema image to quickly visualize it

---
+++ database/migrations/20200918192609_01-schema.js
@@ -0,0 +1,39 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('users', users => {
+        users.increments()
+
+        users.text('first_name')
+            .notNullable()
+        users.text('last_name')
+            .notNullable()
+        users.text('email')
+            .notNullable()
+            .unique()
+            .index()
+        users.text('password')
+            .notNullable()
+            .unique()
+            .index()
+
+    })
+        .createTable('comments', comments => {
+            comments.increments()
+
+            users.text('comment')
+                .notNullable()
+                .index()
+            users.text('negativity')
+                .notNullable()
+                .unique()
+            comments.foreign('user_id')
+                .references('users.id')
+                .unique()
+
+        }
+        )
+}
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('comments').dropTableIfExists('users')
+}
