1c18f2a14d875134c1b5975e0064535b90968a9b
royeradames <royeraadames@gmail.com>
2020-09-20 03:29:08 -0400

    completed comments model

---
+++ comments/comments-model.js
@@ -0,0 +1,39 @@
+const db = require("../database/dbConfig")
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+}
+
+
+function find() {
+  return db("comments").select('*').orderBy("id")
+}
+
+function findBy(filter) {
+  // console.log(`-- user model inside findBy --`)
+  // console.log(filter)
+
+  return db("comments")
+    .where(filter)
+    .orderBy("id")
+}
+
+
+async function add(user) {
+  try {
+    const [id] = await db("comments").insert(user, "id")
+
+    return findById(id)
+  } catch (error) {
+    throw error
+  }
+}
+
+function findById(id) {
+  return db("comments")
+  .where({ id })
+  .first()
+}
