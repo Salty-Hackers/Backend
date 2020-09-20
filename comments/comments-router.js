632a216acca8826e8b89a48a212b382da73e10a4
royeradames <royeraadames@gmail.com>
2020-09-20 16:01:59 -0400

    update delete comment model, and working on the update comment endpoint

--- comments/comments-router.js
+++ comments/comments-router.js
@@ -3,6 +3,7 @@ const router = require("express").Router()
 const Comments = require("./comments-model")
 const restricted = require("../auth/authenticate-middleware")
 
+// users endpoints
 router.get("/", restricted, (req, res, next) => {
 
   // console.log('users get /')
@@ -24,7 +25,7 @@ router.get("/", restricted, (req, res, next) => {
     .catch(next)
 })
 
-router.get('/:id',  restricted, validateId, (req, res, next) => {
+router.get('/:id', restricted, validateId, (req, res, next) => {
   // console.log('users get /')
   // console.log(req.jwt)
   // console.log(req.jwt.department)
@@ -45,11 +46,11 @@ router.get('/:id',  restricted, validateId, (req, res, next) => {
 })
 
 router.post('/', restricted, validateData, (req, res, next) => {
-    Comments.add(req.body)
+  Comments.add(req.body)
     .then((addedComment) => {
 
-    //   console.log(`inside findBy`)
-    //   console.log(users)
+      //   console.log(`inside findBy`)
+      //   console.log(users)
 
       if (addedComment) {
         res.status(200).json(addedComment)
@@ -61,38 +62,44 @@ router.post('/', restricted, validateData, (req, res, next) => {
 })
 router.delete("/:id", restricted, validateId, (req, res, next) => {
 
-    // console.log('users get /')
-    // console.log(req.jwt)
-    // console.log(req.jwt.department)
-  
-    Users.deleteUser(req.params.id)
-      .then((comment) => {
-  
-        // console.log(`inside findBy`)
-        // console.log(users)
-  
-        if (comment) {
-          //todo: take away the returning password
-          res.status(200).json(comment)
-        } else {
-          res.status(404).json({ message: 'Invalid comment id' })
-        }
-      })
-      .catch(next)
-  })
-function validateData (req, res, next) {
+  // console.log('users get /')
+  // console.log(req.jwt)
+  // console.log(req.jwt.department)
+
+  Users.deleteUser(req.params.id)
+    .then((comment) => {
+
+      // console.log(`inside findBy`)
+      // console.log(users)
+
+      if (comment) {
+        //todo: take away the returning password
+        res.status(200).json(comment)
+      } else {
+        res.status(404).json({ message: 'Invalid comment id' })
+      }
+    })
+    .catch(next)
+})
+
+router.put("/:id", restricted, validateId, (req, res, next) => {
+
+})
+
+// local middleware
+  function validateData(req, res, next) {
     if (!req.body.comment && !req.body.negativity && !req.body.user_id) {
-        res.status(404).json({error: `comment, negativity, and user_id are require`})
+      res.status(404).json({ error: `comment, negativity, and user_id are require` })
     }
     next()
-}
-function validateId (req, res, next) {
+  }
+function validateId(req, res, next) {
     Users.deleteUser(req.params.id)
       .then((comment) => {
         if (comment) {
           next()
         } else {
-          res.status(404).json({error: `Invalid ID`})
+          res.status(404).json({ error: `Invalid ID` })
         }
       })
       .catch(next)
