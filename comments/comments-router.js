695be05c1eb77e52134d22e320b859a1546c0b03
royeradames <royeraadames@gmail.com>
2020-09-20 03:39:52 -0400

    complete comments router

--- comments/comments-router.js
+++ comments/comments-router.js
@@ -23,31 +23,12 @@ router.get("/",  (req, res, next) => {
     .catch(next)
 })
 
-router.get('/comments',  (req, res, next) => {
-  // console.log('users get /')
-  // console.log(req.jwt)
-  // console.log(req.jwt.department)
-
-  Comments.findUsersComments()
-    .then(users => {
-
-      // console.log(`inside findBy`)
-      // console.log(users)
-
-      if (users.length) {
-        res.status(200).json(users)
-      } else {
-        res.status(404).json({ message: 'no users at the moment' })
-      }
-    })
-    .catch(next)
-})
 router.post('/:id',  (req, res, next) => {
   // console.log('users get /')
   // console.log(req.jwt)
   // console.log(req.jwt.department)
 
-  Comments.findAUserComments(req.params.id)
+  Comments.findById(req.params.id)
     .then((users) => {
 
       console.log(`inside findBy`)
