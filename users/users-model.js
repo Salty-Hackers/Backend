2f701debaa959f05e2443994f5c6306a39b94aea
royeradames <royeraadames@gmail.com>
2020-09-20 02:53:23 -0400

    took away userComments parameters

--- users/users-model.js
+++ users/users-model.js
@@ -7,7 +7,7 @@ module.exports = {
   findById,
   userComments,
 }
-function userComments (id) {
+function userComments () {
   return db("users as u")
   .select("u.id", "u.first_name as firstName", 'u.last_name as lastName', 'u.email', 'c.comment', 'c.negativity as negativityScore')
   .join('comments as c', 'u.id', 'c.user_id') 
