0234f9b8c1c9611f90f2ddffa61f731ef91c58de
royeradames <royeraadames@gmail.com>
2020-09-20 03:47:36 -0400

    complete validateData local middleware

--- comments/comments-router.js
+++ comments/comments-router.js
@@ -23,7 +23,7 @@ router.get("/",  (req, res, next) => {
     .catch(next)
 })
 
-router.post('/:id',  (req, res, next) => {
+router.get('/:id',  (req, res, next) => {
   // console.log('users get /')
   // console.log(req.jwt)
   // console.log(req.jwt.department)
@@ -43,5 +43,27 @@ router.post('/:id',  (req, res, next) => {
     .catch(next)
 })
 
+router.post('/', validateData, (req, res, next) => {
+    Comments.add(req.body)
+    .then((addedComment) => {
+
+    //   console.log(`inside findBy`)
+    //   console.log(users)
+
+      if (addedComment) {
+        res.status(200).json(addedComment)
+      } else {
+        res.status(404).json({ message: 'no user comments found' })
+      }
+    })
+    .catch(next)
+})
+
+function validateData (req, res, next) {
+    if (!req.body.comment && !req.body.negativityScore && !req.body.user_id) {
+        res.status(404).json({error: `comment, negative score, and the id of the user who made the comment`})
+    }
+    next()
+}
 
 module.exports = router
