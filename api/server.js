1b7cf2612cb6feaf8d3903ba3e3ecb62e34d7d2a
royeradames <royeraadames@gmail.com>
2020-09-21 12:37:40 -0400

    added comments to explain what the server.js is doing

--- api/server.js
+++ api/server.js
@@ -1,17 +1,24 @@
+// libraries import
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+// file import
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const userRouter = require(`../users/users-router`)
 const commentsRouter = require(`../comments/comments-router`)
 
+// start the server
 const server = express();
 
+
+// third-party middleware
 server.use(helmet());
 server.use(cors());
+//built in middleware
 server.use(express.json());
+// error middleware
 server.use((err, req, res, next) => {
     console.log(err)
 
@@ -21,6 +28,7 @@ server.use((err, req, res, next) => {
 }
 );
 
+// custom middleware
 server.use('/api/auth', authRouter);
 server.use('/api/users', authenticate, userRouter);
 server.use('/api/comments', authenticate, commentsRouter);
