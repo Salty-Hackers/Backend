10b275ec392bde129c0f71dc4ddd18573ff72180
royeradames <royeraadames@gmail.com>
2020-09-21 19:59:38 -0400

    working on auth router testing

--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -1,9 +1,71 @@
 const supertest = require("supertest")
 
-const authRouter = require("./auth-router")
+const authRouter = require("../api/server")
 const db = require("../database/dbConfig")
+const { expectCt } = require("helmet")
 
 describe('authRouter', () => {
+    beforeAll(async () => {
+        // trucate or empty the hobbits table
+        // await db("users").truncate()
+        // how to migrate, and run seed on js
+        // await db.migrate.rollback()
+        // .then(() => db.migrate.latest())
+        // .then(() => db.seed.run());
+    })
+    describe(`post /singup`, () => {
+        const newUserValidData = {
+            first_name: 'royer',
+            last_name: 'adames',
+            email: 'royeraadames@gmail.com',
+            password: 'admin',
+        }
 
-}
-) 
+        it(`should respond 200 when right amount of data is given`, () => {
+            // expect(true).toBe(true)
+
+            return supertest(authRouter)
+                .post(`/api/auth/signup`)
+                .send({
+                    first_name: 'royer',
+                    last_name: 'adames',
+                    email: 'royeraadames@gmail.com',
+                    password: 'admin',
+                })
+                .then(res => {
+                    expect(res.status).toBe(404)
+                    // expect(res.body.message).toMatch(/User sucessfully made./i)
+                    console.log(res.body)
+                })
+        })
+        it(`should respond 404 when password is not a string`, () => {
+
+        })
+        it(`should respond 404 when there is no password`, () => {
+
+        })
+        it(`should respond 404 when there is no email`, () => {
+
+        })
+        it(`should respond 404 when there is no first_name`, () => {
+
+        })
+        it(`should respond 404 when there is no last_name`, () => {
+
+        })
+    })
+    describe(`post /login`, () => {
+        it(``, () => {
+
+        })
+        it(``, () => {
+
+        })
+        it(``, () => {
+
+        })
+        it(``, () => {
+
+        })
+    })
+}) 
--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,11 @@ module.exports = {
       tableName: 'dbmigrations',
     },
     seeds: { directory: './database/seeds' },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done)
+      }
+    }
   },
   testing: {
     client: "sqlite3",
