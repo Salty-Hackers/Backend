864a9685c7279d106bf494a41d1335306d56d360
royeradames <royeraadames@gmail.com>
2020-09-21 18:35:50 -0400

    starting tests. first authentication router

---
+++ auth/auth-router.test.js
@@ -0,0 +1,9 @@
+const supertest = require("supertest")
+
+const authRouter = require("./auth-router")
+const db = require("../database/dbConfig")
+
+describe('authRouter', () => {
+
+}
+) 
---
+++ users/users-router.test.js
@@ -0,0 +1,9 @@
+const supertest = require("supertest")
+
+const usersRouter = require("./users-router")
+const db = require("../database/dbConfig")
+
+describe('usersRouter', () => {
+
+}
+)
