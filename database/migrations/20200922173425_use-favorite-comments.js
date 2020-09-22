3d45f037a0930fb35a9953ca25703afa52d2d86f
royeradames <royeraadames@gmail.com>
2020-09-22 17:49:25 -0400

    completed new migration for comment favorite column

--- database/migrations/20200922173425_use-favorite-comments.js
+++ database/migrations/20200922173425_use-favorite-comments.js
@@ -1,10 +1,12 @@
 
 exports.up = function (knex) {
-    return knex.schema.table('users', users => {
-        users.bool('favorite')
+    return knex.schema.table('comments', comments => {
+        comments.boolean('favorite')
     })
 };
 
 exports.down = function (knex) {
-
-};
+    return knex.schema.table('comments', comments => {
+        comments.dropColumn('favorite')
+    })
+}
