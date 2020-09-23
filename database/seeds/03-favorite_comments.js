2f62e22fd45e8e07b190cf761ca9a12d62e1d017
royeradames <royeraadames@gmail.com>
2020-09-23 16:10:16 -0400

    update the table name for favorite_commend seed data. Run migrate latest and seed

--- database/seeds/03-favorite_comments.js
+++ database/seeds/03-favorite_comments.js
@@ -4,7 +4,7 @@ faker.seed(4321)
 exports.seed = function (knex) {
 
   // Inserts seed entries
-  return knex('table_name').insert([
+  return knex('favorite_comments').insert([
     {
       user_id: faker.random.number({ max: 10, min: 1, precision: 1 }),
       comment_id: faker.random.number({ max: 16, min: 1, precision: 1 }),
