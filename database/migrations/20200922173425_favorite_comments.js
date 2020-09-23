3b92a0bb76db380ccac078bed713e94f530d2661
royeradames <royeraadames@gmail.com>
2020-09-23 15:56:26 -0400

    update the name of the table to the correct name

--- database/migrations/20200922173425_favorite_comments.js
+++ database/migrations/20200922173425_favorite_comments.js
@@ -1,6 +1,6 @@
 
 exports.up = function (knex) {
-    return knex.schema.createTable('comments', comments => {
+    return knex.schema.createTable('favorite_comments', comments => {
         comments.increments()
 
         comments.integer('user_id')
