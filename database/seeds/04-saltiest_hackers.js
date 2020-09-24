8447e62c623007a8ee976e058457f499e7e20850
royeradames <royeraadames@gmail.com>
2020-09-24 17:50:27 -0400

    completed saltiest_hackers seed file, now to testing and refactoring

---
+++ database/seeds/04-saltiest_hackers.js
@@ -0,0 +1,30 @@
+const axios = require('axios')
+async function getDSSaltiestHackers() {
+  try {
+    //get data from the DS app
+    const res = await axios.post(`https://salted-hacker-news.herokuapp.com/saltiest-hackers`, {
+      num_hackers: 100,
+      min_comments: 1
+    })
+
+    //rebuild data in a way that the table accepts
+
+    const saltiestHackerData = []
+    for (const property in res) {
+      saltiestHackerData.push({
+        username: res[property][0],//username
+        total_score: res[property][1],//total_Score
+      })
+    }
+    return saltiestHackerData
+  } catch (error) {
+    throw error
+  }
+}
+
+
+exports.seed = function (knex) {
+
+  // Inserts seed entries
+  return knex('saltiest_hackers').insert(getDSSaltiestHackers());
+};
